6c83df8aeae80304e1d01c8d8eccea9c6455b0d2
Thalys001 <thalyssouza708@gmail.com>
2023-08-24 23:42:55 -0300

    feat: adding buttontex component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,16 @@
 import { Container, Links } from './styles';
 
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir nota"/>
       <Section title='Links úteis'>
         <Links>
           <li>
@@ -19,7 +21,6 @@ export function Details() {
           </li>
         </Links>
       </Section>
-
       <Section title='Marcadores'>
         <Tag title="express" />
         <Tag title="node" />
