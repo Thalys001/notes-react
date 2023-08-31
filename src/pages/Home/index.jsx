7c2cb644c2bbcecc6db6a5a6300ac9bb6194e972
Thalys001 <thalyssouza708@gmail.com>
2023-08-30 23:58:47 -0300

    feat: adding create note button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
@@ -14,7 +15,7 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive/>
+          <ButtonText title="Todos" $isactive />
         </li>
         <li>
           <ButtonText title="React" />
@@ -34,7 +35,8 @@ export function Home() {
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
     </Container>
   )
