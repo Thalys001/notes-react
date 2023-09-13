793fa1b5076c7da84a1531882115bb5fc0c2a707
Thalys001 <thalyssouza708@gmail.com>
2023-09-12 23:04:06 -0300

    feat: adding avatar component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
   return (
@@ -12,6 +12,19 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img
+            src="https://github.com/Thalys001.png"
+            alt="foto do usuário"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input
+              id='avatar'
+              type="file"
+            />
+          </label>
+        </Avatar>
         <Input
           placeholder="Nome"
           type="text"
