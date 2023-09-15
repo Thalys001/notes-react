692cabd81b16c6349e79dd18bdee67b4b2ded5a2
Thalys001 <thalyssouza708@gmail.com>
2023-09-14 22:45:47 -0300

    feat: adding form in new interface

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,26 @@
-import {Header} from '../../components/Header'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
-import {Container} from './styles';
+import { Container, Form } from './styles';
 
-export function New(){
+export function New() {
   return (
     <Container>
-      <Header/>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>
+              Criar nota
+            </h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Titulo" />
+
+        </Form>
+      </main>
     </Container>
   )
 }
