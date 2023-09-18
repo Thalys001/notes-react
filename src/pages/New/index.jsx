a239c91466aa2b5a70eb3233fe81c965438e5326
Thalys001 <thalyssouza708@gmail.com>
2023-09-17 23:53:49 -0300

    feat: finalizing new component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,9 @@
+import { Section } from '../../components/Section'
 import { Header } from '../../components/Header'
-import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
-import { Section } from '../../components/Section'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 
 import { Container, Form } from './styles';
 
@@ -23,9 +24,17 @@ export function New() {
           <Input placeholder="Titulo" />
           <Textarea placeholder="observações" />
           <Section title="Links úteis">
-            <NoteItem value="https://github.com/Thalys001"/>
-            <NoteItem isNew placeholder="Novo link"/>
+            <NoteItem value="https://github.com/Thalys001" />
+            <NoteItem isNew placeholder="Novo link" />
           </Section>
+          <Section title="Marcadores">
+            <div className='tags'>
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar"/>
         </Form>
       </main>
     </Container>
